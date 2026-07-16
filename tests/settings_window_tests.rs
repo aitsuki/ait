83dc7512caab3922d211b245530ef2715b19f763
aitsuki <aitsukigo@gmail.com>
2026-07-17 00:42:19 +0800

    chore: release v0.2.5

--- tests/settings_window_tests.rs
+++ tests/settings_window_tests.rs
@@ -633,7 +633,7 @@ fn settings_window_layout_places_auto_start_with_global_settings() {
 
 #[test]
 fn app_version_text_uses_package_version() {
-    assert_eq!(app_version_text(), "ait v0.2.4");
+    assert_eq!(app_version_text(), "ait v0.2.5");
 }
 
 #[test]
